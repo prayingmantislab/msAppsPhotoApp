6fe2d8842d38e57ec6d533ed06c8d51442dabe90
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2023-11-29 19:35:29 +0200

    fixed center layout

--- client/src/components/PhotoList.tsx
+++ client/src/components/PhotoList.tsx
@@ -9,10 +9,12 @@ interface PhotoListProps {
   onPhotoClick: (photo: Photo) => void;
 }
 const PhotoList: React.FC<PhotoListProps> = ({ photos, onPhotoClick }) => (
-  <div className='grid grid-cols-3 space-x-2 space-y-2 h-screen'>
-    {photos.slice(0, 9).map((photo) => (
-      <Photo key={photo.id} photo={photo} onClick={onPhotoClick} />
-    ))}
+  <div className='flex justify-center'>
+    <div className='grid grid-cols-3 gap-4 max-w-screen-md'>
+      {photos.slice(0, 9).map((photo) => (
+        <Photo key={photo.id} photo={photo} onClick={onPhotoClick} />
+      ))}
+    </div>
   </div>
 );
 
