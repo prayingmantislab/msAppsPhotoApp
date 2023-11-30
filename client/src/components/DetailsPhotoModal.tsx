7f62ac9443150993e367edf6c817901848d48a9e
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2023-11-30 16:43:56 +0200

    added a button to the modal

--- client/src/components/DetailsPhotoModal.tsx
+++ client/src/components/DetailsPhotoModal.tsx
@@ -4,28 +4,23 @@ import { setSelectedPhoto } from '../store';
 import { RootState } from '../store';
 
 const DetailsPhotoModal: React.FC = () => {
-  // Get the selectedPhoto from the Redux state
   const { selectedPhoto } = useSelector((state: RootState) => ({
     selectedPhoto: state.selectedPhoto,
   }));
   const dispatch = useDispatch();
 
-  // Define a function to close the modal
   const handleClose = () => {
-    // Dispatch the setSelectedPhoto action with null to unselect the photo
     dispatch(setSelectedPhoto(null));
   };
 
   return (
-    // Show the modal if a photo is selected
     <Modal isOpen={selectedPhoto !== null} onRequestClose={handleClose}>
-      {/* If a photo is selected, show its details */}
       {selectedPhoto && (
-        <div className='flex flex-col items-center space-y-4'>
+        <div className='bg-white rounded-lg h-auto flex flex-col items-center space-y-4'>
           <img
             src={selectedPhoto.largeImageURL}
             alt=''
-            className='w-full h-auto'
+            className='w-1/2 h-1/2'
           />
           <h2 className='text-xl font-bold'>Photo Details</h2>
           <p>ID: {selectedPhoto.id}</p>
@@ -34,6 +29,12 @@ const DetailsPhotoModal: React.FC = () => {
           <p>Likes: {selectedPhoto.likes}</p>
           <p>Image Width: {selectedPhoto.imageWidth}</p>
           <p>Image Height: {selectedPhoto.imageHeight}</p>
+          <button
+            onClick={handleClose}
+            className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+          >
+            Close
+          </button>
         </div>
       )}
     </Modal>
